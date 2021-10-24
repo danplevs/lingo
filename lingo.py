0f4295f6f66c9ec5ef2939ec68a278a0fbe9c042
danpl <daniel.re4@gmail.com>
2021-10-24 18:04:30 -0300

    change part of speech data structure to a single dict

--- lingo.py
+++ lingo.py
@@ -2,7 +2,8 @@ from abc import ABC, abstractmethod
 from spacy import load, explain
 from spacy.language import Language
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Dict
+from collections import defaultdict
 
 
 class NLPModel(ABC):
@@ -23,17 +24,20 @@ class NLP:
     text: str
     model: NLPModel
     nlp: Language = field(init=False, repr=False)
-    words: List[str] = field(init=False, repr=False)
-    lemma: List[str] = field(init=False, repr=False)
-    pos: List[str] = field(init=False, repr=False)
-    detail: List[str] = field(init=False, repr=False)
+    part_of_speech: Dict[str, List[str]] = field(init=False, default_factory=lambda: defaultdict(list))
     
     def __post_init__(self):
         self.nlp = self.model.load_model()
+        
     
     def process(self):
         doc = self.nlp(self.text)
         for token in doc:
+            self.part_of_speech["words"].append(token.text)
+            self.part_of_speech["lemma"].append(token.lemma_)
+            self.part_of_speech["pos"].append(token.pos_)
+            self.part_of_speech["detail"].append(explain(token.tag_))
+            
             print(token.text, token.lemma_, token.pos_, explain(token.tag_))
 
 class Translator:
