89f024ef1d9ed2b7085d3af1ba1b5a14283fa788
danpl <daniel.re4@gmail.com>
2021-10-27 22:30:31 -0300

    make NPL a behavioural class

--- lingo.py
+++ lingo.py
@@ -1,80 +1,85 @@
 """Main module from the language learning helper.
 Contains NLP and translation functionalities, and the data container objects."""
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
 from typing import List, DefaultDict, Sequence
 from collections import defaultdict
 import spacy
 from spacy import displacy
 
 
-class LanguageModel(ABC):
-    """Basic representation of a specific NLP model."""
-    language: str
+class Language(ABC):
+    """Basic representation of a language NLP model."""
+
+    iso_639_1: str
 
     @classmethod
     @abstractmethod
     def load_model(cls) -> spacy.language.Language:
         """Load model pipeline"""
 
-class ENModel(LanguageModel):
-    language = 'en'
 
+class English(Language):
     """English NLP model."""
+
+    iso_639_1 = "en"
+
     @classmethod
     def load_model(cls) -> spacy.language.Language:
         return spacy.load("en_core_web_sm")
 
-class DEModel(LanguageModel):
+
+class German(Language):
     """German NLP model."""
-    language = 'de'
+
+    iso_639_1 = "de"
 
     @classmethod
     def load_model(cls) -> spacy.language.Language:
         return spacy.load("de_core_news_sm")
 
-@dataclass
+
 class NLP:
     """NLP processor and renderer."""
-    text: Sequence[str]
-    model: LanguageModel
-    nlp: spacy.language.Language = field(init=False, repr=False)
-    doc: spacy.tokens.Doc = field(init=False, repr=False)
 
-    def __post_init__(self):
-        if not isinstance(self.text, str):
-            self.text = ' '.join(self.text)
-        self.nlp = self.model.load_model()
-        self.doc = self.nlp(self.text)
+    def __init__(self, language: Language) -> None:
+        self.nlp: spacy.language.Language = language.load_model()
 
-    def process(self) -> DefaultDict[str, List[str]]:
+    def process(self, text: Sequence[str]) -> DefaultDict[str, List[str]]:
         """Process part of speech tags."""
+        docs: List[spacy.tokens.Doc] = (
+            [self.nlp(text)]
+            if isinstance(text, str)
+            else [self.nlp(phrase) for phrase in text]
+        )
+
         part_of_speech: DefaultDict[str, List[str]] = defaultdict(list)
-        for token in self.doc:
-            if not token.is_punct:
-                part_of_speech["word"].append(token.text)
-                part_of_speech["lemma"].append(token.lemma_)
-                part_of_speech["detail"].append(spacy.explain(token.tag_))
+        for doc in docs:
+            for token in doc:
+                if not token.is_punct:
+                    part_of_speech["word"].append(token.text)
+                    part_of_speech["lemma"].append(token.lemma_)
+                    part_of_speech["detail"].append(spacy.explain(token.tag_))
 
         return part_of_speech
 
-    def plot_dependencies(self) -> str:
-        """Render a dependency parse tree."""
-        return displacy.render(self.doc, style='dep')
+    def plot_phrase(self, phrase) -> str:
+        """Render a dependency parse tree of a phrase."""
+        return displacy.serve(self.nlp(phrase), style="dep")
 
 
 class Lesson:
     pass
 
+
 class Class:
     pass
 
+
 def main():
     """Main function. It's being used only for testing purposes for now."""
-    test = NLP("Doing some tests.", DEModel)
+    test = NLP(German)
+    print(test.process(("Testing",)))
 
-    print(test.process())
-    print(type(test.text))
 
 if __name__ == "__main__":
     main()
