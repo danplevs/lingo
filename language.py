035a1c1db2f2217d6d409503197c0a0e730da18b
danpl <daniel.re4@gmail.com>
2021-12-26 13:20:41 -0300

    add iso639-1

--- language.py
+++ language.py
@@ -10,19 +10,25 @@ with LANGUAGE_DATA_PATH.open(encoding="utf-8") as file:
 
 class Language():
     """Basic NLP language model object."""
+    iso_639_1: str
+
     @classmethod
     def load_model(cls) -> spacy.language.Language:
-        """Load model pipeline"""
+        """Load model pipeline."""
         model_name = languages[cls.__name__.lower()]["model"]
         return spacy.load(model_name)
 
 
 class English(Language):
     """English NLP model."""
+    iso_639_1 = languages["english"]["iso_639-1"]
 
 
 class German(Language):
     """German NLP model."""
-    
+    iso_639_1 = languages["german"]["iso_639-1"]
+
+
 class Portuguese(Language):
     """Portuguese NLP model."""
+    iso_639_1 = languages["portuguese"]["iso_639-1"]
