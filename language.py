9d7ed783067458656f3f0698be8a0996cfd8a48c
danpl <daniel.re4@gmail.com>
2021-12-27 17:18:50 -0300

    simplify language data

--- language.py
+++ language.py
@@ -1,34 +1,28 @@
 """Module containing language pipeline objects"""
 import json
 from pathlib import Path
-import spacy
+import spacy.language
 
-LANGUAGE_DATA_PATH = Path("./language.json")
+
+LANGUAGE_DATA_PATH = Path("./models.json")
 
 with LANGUAGE_DATA_PATH.open(encoding="utf-8") as file:
-    languages = json.load(file)
+    models = json.load(file)
 
 class Language():
     """Basic NLP language model object."""
-    iso_639_1: str
 
     @classmethod
     def load_model(cls) -> spacy.language.Language:
-        """Load model pipeline."""
-        model_name = languages[cls.__name__.lower()]["model"]
+        """Load a specific language model pipeline, derived from the class name."""
+        model_name = models[cls.__name__.lower()]
         return spacy.load(model_name)
+    
+
+class English(Language): """English NLP model."""
 
 
-class English(Language):
-    """English NLP model."""
-    iso_639_1 = languages["english"]["iso_639-1"]
+class German(Language): """German NLP model."""
 
 
-class German(Language):
-    """German NLP model."""
-    iso_639_1 = languages["german"]["iso_639-1"]
-
-
-class Portuguese(Language):
-    """Portuguese NLP model."""
-    iso_639_1 = languages["portuguese"]["iso_639-1"]
+class Portuguese(Language): """Portuguese NLP model."""
