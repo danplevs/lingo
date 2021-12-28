cd4d39cf36a5a2612e5343e71592d4bafcd653d0
danpl <daniel.re4@gmail.com>
2021-12-28 19:15:39 -0300

    fix text_to_speech() return value

--- src/speech.py
+++ src/speech.py
@@ -8,4 +8,5 @@ def text_to_speech(text: str):
     """Convert text into a natural sounding speech."""
     mp3_fp = BytesIO()
     tts = gTTS(text, lang=detect_language(text))
-    return tts.write_to_fp(mp3_fp)
+    tts.write_to_fp(mp3_fp)
+    return mp3_fp
