df35c23abfe35efb7546ee29788d3035c36dca76
danpl <daniel.re4@gmail.com>
2021-12-29 13:44:51 -0300

    refactor and add syntactic dependency to process()

--- src/nlp.py
+++ src/nlp.py
@@ -1,38 +1,45 @@
 """NLP module containing pos tagging, translation and text to speech functions."""
-from typing import List, DefaultDict, Sequence
+from typing import List, DefaultDict
 from collections import defaultdict
 
-import spacy.language
+import spacy
+from spacy.tokens import Doc
+import pandas as pd
 from language import Language, match_language
 from translation import detect_language
 
 
 class NLP:
     """NLP processor and renderer."""
+
     def __init__(self, text: str) -> None:
         self.text: str = text
         self.language: Language = match_language(detect_language(text))
         self.nlp: spacy.language.Language = self.language.load_model()
 
-    def process(self, text: Sequence[str]) -> DefaultDict[str, List[str]]:
+    def process(self) -> DefaultDict[str, List[str]]:
         """Process part of speech tags."""
-        docs: List[spacy.tokens.Doc] = (
-            [self.nlp(text)]
-            if isinstance(text, str)
-            else [self.nlp(phrase) for phrase in text]
-        )
+        doc: Doc = self.nlp(self.text)
 
         part_of_speech: DefaultDict[str, List[str]] = defaultdict(list)
-        for doc in docs:
-            for token in doc:
-                if not token.is_punct:
-                    part_of_speech["word"].append(token.text)
-                    part_of_speech["lemma"].append(token.lemma_)
-                    part_of_speech["detail"].append(spacy.explain(token.tag_))
+        for token in doc:
+            part_of_speech["Text"].append(token.text)
+            part_of_speech["Lemma"].append(token.lemma_)
+            part_of_speech["Part of speech"].append(
+                spacy.explain(token.tag_) + f" ({token.pos_})"
+            )
+            part_of_speech["Syntactic dependency"].append(
+                token.dep_.lower()
+                if spacy.explain(token.dep_) is None
+                else spacy.explain(token.dep_) + f" ({token.dep_})"
+            )
 
         return part_of_speech
 
-print(NLP("Ich bin eine frau").text)
-print(NLP("Ich bin eine frau").language)
-print(NLP("Ich bin eine frau").nlp)
-print(NLP("Ich bin eine frau").process("Ich bin eine frau"))
+
+nlp = NLP("Ich bin eine frau")
+
+print(nlp.text)
+print(nlp.language)
+print(nlp.nlp)
+print(nlp.process())
