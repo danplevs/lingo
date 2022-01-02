6819a703e5411029fcbea429f9ba56b74a5a3f07
danpl <daniel.re4@gmail.com>
2022-01-02 14:23:38 -0300

    fix path handling

--- src/config.py
+++ src/config.py
@@ -3,7 +3,8 @@ import os
 from pathlib import Path
 import dotenv
 
+ROOT = Path(__file__).parents[1]
 
 dotenv.load_dotenv(dotenv.find_dotenv())
-GOOGLE_CREDENTIALS_PATH = Path(os.getenv("GOOGLE_CREDENTIALS_PATH"))
-LANGUAGE_DATA_PATH = Path(os.getenv("LANGUAGE_DATA_PATH"))
+GOOGLE_CREDENTIALS_PATH = ROOT / os.getenv("GOOGLE_CREDENTIALS_PATH")
+LANGUAGE_DATA_PATH = ROOT / os.getenv("LANGUAGE_DATA_PATH")
