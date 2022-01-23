1deab4a052d26aaf481470efadc618ab439aebb9
danplevs <daniel.re4@gmail.com>
2022-01-22 23:44:58 -0300

    change fetch model to use iso code

--- src/language.py
+++ src/language.py
@@ -1,16 +1,15 @@
-"""Module containing language pipeline objects"""
+"""Module containing languages and models."""
 import json
 from typing import Dict
-import iso639
 from config import LANGUAGE_DATA_PATH
+import iso639
 
 
 with LANGUAGE_DATA_PATH.open(encoding="utf-8") as file:
     MODELS: Dict[str, str] = json.load(file)
 
-LANGUAGES_SUPPORTED = sorted([lang.title() for lang in MODELS.keys()])
+LANGUAGES_SUPPORTED = {lang:iso639.to_name(lang).title() for lang in MODELS.keys()}
 
-def fetch_model(language: str):
+def fetch_model(language_code: str):
     """Helper function to fetch the spacy model name that matches text language."""
-    lang_name = iso639.to_name(language).lower()
-    return MODELS[lang_name]
+    return MODELS[language_code]
